43b6593875ecd89b26cdffad4f4497fe3ba12a82
mathieuh64400 <mhedot@neuf.fr>
2021-04-28 14:43:11 +0200

    commit aprés réalisation du slider

--- JeuMVC Structure/controller/Etape2.js
+++ JeuMVC Structure/controller/Etape2.js
@@ -1,51 +1,63 @@
 import Controller from "../core/Controller.js"
 export default class Etape2 extends Controller {
-    constructor (){
-        super();
-        document.title="Etape du jeu 2"
-        console.log("jeu:etape2");
-        console.log(this.state);
-        // Select the carousel you'll need to manipulate and the buttons you'll add events to
-const carousel = document.querySelector("[data-target='carousel']");
-const card = carousel.querySelector("[data-target='card']");
-const leftButton = document.querySelector("[data-action='slideLeft']");
-const rightButton = document.querySelector("[data-action='slideRight']");
+  constructor() {
+    super();
+    document.title = "Etape du jeu 2"
+    console.log("jeu:etape2");
+    console.log(this.state);
 
-// Prepare to limit the direction in which the carousel can slide, 
-// and to control how much the carousel advances by each time.
-// In order to slide the carousel so that only three cards are perfectly visible each time,
-// you need to know the carousel width, and the margin placed on a given card in the carousel
-const carouselWidth = carousel.offsetWidth;
-const cardStyle = card.currentStyle || window.getComputedStyle(card)
-const cardMarginRight = Number(cardStyle.marginRight.match(/\d+/g)[0]);
+    const span = document.getElementsByTagName('span');
+    const div = document.getElementsByTagName('div');
+    var l = 0;
+    span[1].onclick = () => {
+      l++;
+      for (var i of div) {
+        if (l == 0) {
+          i.style.left = "0px";
+        }
+        if (l == 1) {
+          i.style.left = "-740px";
+        }
+        if (l == 2) {
+          i.style.left = "-1480px";
+        }
+        if (l == 3) {
+          i.style.left = "-2220px";
+        }
+        if (l == 4) {
+          i.style.left = "-2960px";
+        }
 
-// Count the number of total cards you have
-const cardCount = carousel.querySelectorAll("[data-target='card']").length;
-
-// Define an offset property to dynamically update by clicking the button controls
-// as well as a maxX property so the carousel knows when to stop at the upper limit
-let offset = 0;
-const maxX = -((cardCount / 3) * carouselWidth + 
-               (cardMarginRight * (cardCount / 3)) - 
-               carouselWidth - cardMarginRight);
-
-
-// Add the click events
-leftButton.addEventListener("click", function() {
-  if (offset !== 0) {
-    offset += carouselWidth + cardMarginRight;
-    carousel.style.transform = `translateX(${offset}px)`;
+        if (l > 4) {
+          l = 4;
+        }
+      }
     }
-})
-  
-rightButton.addEventListener("click", function() {
-  if (offset !== maxX) {
-    offset -= carouselWidth + cardMarginRight;
-    carousel.style.transform = `translateX(${offset}px)`;
+    span[0].onclick = () => {
+      l--;
+      for (var i of div) {
+        if (l == 0) {
+          i.style.left = "0px";
+        }
+        if (l == 1) {
+          i.style.left = "-740px";
+        }
+        if (l == 2) {
+          i.style.left = "-1480px";
+        }
+        if (l == 3) {
+          i.style.left = "-2220px";
+        }
+        if (l == 4) {
+          i.style.left = "-2960px";
+        }
+        if (l < 0) {
+          l = 4;
+        }
+      }
+    }
   }
-})
-      
-      
-        
-    }
+
+
+
 }

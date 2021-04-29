2231bb115010013e1d83172ca1afa314be576673
mathieuh64400 <mhedot@neuf.fr>
2021-04-29 09:26:00 +0200

    commit aprés réalisation du slider+cadre

--- JeuMVC Structure/controller/Etape2.js
+++ JeuMVC Structure/controller/Etape2.js
@@ -56,6 +56,66 @@ export default class Etape2 extends Controller {
         }
       }
     }
+
+    const listeUserStories = document.querySelectorAll("article[data-selected]");
+    console.log(listeUserStories);
+
+    listeUserStories.forEach(listeUserStorie => {
+      listeUserStorie.addEventListener('click', selectionner);
+    });
+
+    function selectionner() {
+      this.dataset.selected = this.dataset.selected == "true" ? "false" : "true";
+
+      // this.dataset.selected = this.dataset.selected == "true" ? this.dataset.cadreselected=="false" : this.dataset.cadreselected=="true";
+
+      alert('votre selection est réussie!!Validez-la en cliquant sur le bouton Finalisez la préparation');
+
+      const selection = document.querySelectorAll('article[data-selected="true"]').length;
+
+      const elementSelected = document.querySelectorAll('article[data-selected="true"]');
+
+      console.log(selection, elementSelected);
+
+
+      // selection de tout les data-cadreref 
+
+      const listecadrerefs = document.querySelectorAll("article[data-cadreselected]");
+      listecadrerefs.forEach(listecadreref => {
+        listecadreref.addEventListener('click', selectionnercadre);
+      });
+
+      function selectionnercadre() {
+        this.dataset.cadreselected = this.dataset.cadreselected == "true" ? "false" : "true";
+        const cadreselected = document.querySelectorAll('article[data-cadreselected="true"]');
+        console.log(cadreselected);
+
+        if (selection == 1 && elementSelected[0] != "" && cadreselected[0] != "") {
+          let titre = document.querySelector("h2");
+          let cadrevert = document.querySelector('article[data-cadreselected="true"]');
+          console.log(cadrevert);
+          cadrevert.style.border = "2em green solid";
+
+          let docResultat = document.createElement("p");
+          docResultat.style.textAlign = "center";
+
+          docResultat.setAttribute("id", "nbre");
+          docResultat.innerHTML = " Bravo !!!! <br> vous avez selectionné votre projet!! <br> Continuez votre préparation"
+          titre.appendChild(docResultat);
+        }
+      }
+
+
+
+    }
+    // let validation = document.querySelectorAll('button[data-link]="/etape3"');
+    // validation.addEventListener("click", actionner());
+
+    // function actionner() {
+    //   this.go("/etape3");
+    // };
+
+
   }
 
 
